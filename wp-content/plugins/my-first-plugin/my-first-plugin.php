cc9bb72338db8583a931e978e2e4986d721b0626
gilons <gf.694765457@gmail.com>
2020-09-08 16:42:00 +0100

    first commit

---
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: My First Plugin
+ * Plugin URI: http://localhost:8080/wordpress/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Fokam Giles
+ * Author URI: http://www.mywebsite.com
+ */
